b1be9a2295ea88391c620d199fb041a9c13e2efe
Paweł Rosiński <dev.rosiv@gmail.com>
2023-12-06 21:13:51 +0100

    WIP

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,4 +1,6 @@
 import typer
+import boto3
+import time
 from typing_extensions import Annotated
 from loguru import logger
 
@@ -6,8 +8,22 @@ app = typer.Typer()
 
 
 @app.command()
-def get_resource_metrics(instance_id: Annotated[str, typer.Option(help="RDS database instance id")]):
+def get_rds_metrics(
+    instance_id: Annotated[str, typer.Option(help="RDS database instance id. It is NOT DBInstanceIdentifier, it is the DB ResourceId")]
+):
     logger.info(f"Hello {instance_id}")
+    pi_client = boto3.client("pi")
+    response = pi_client.get_resource_metrics(
+        ServiceType='RDS',
+        Identifier=instance_id,
+        StartTime=time.time() - 300,
+        EndTime=time.time(),
+        PeriodInSeconds=60,
+        MetricQueries=[{'Metric': 'os.general.numVCPUs.avg'}]
+    )
+
+    logger.debug("response={}", response)
+    return response
 
 
 @app.command()
@@ -16,6 +32,9 @@ def goodbye(name: str, formal: bool = False):
         print(f"Goodbye Ms. {name}. Have a good day.")
     else:
         print(f"Bye {name}!")
+
+
+def get_db_resource_id(instance_id: str):
 
 
 if __name__ == "__main__":
