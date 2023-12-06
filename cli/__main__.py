e59a3161bc49e645a7671c038233e3a06ee811b1
Paweł Rosiński <dev.rosiv@gmail.com>
2023-12-06 20:58:04 +0100

    Initial typer setup

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,10 +1,22 @@
 import typer
+from typing_extensions import Annotated
 from loguru import logger
 
+app = typer.Typer()
 
-def main(name: str):
-    logger.info(f"Hello {name}")
+
+@app.command()
+def get_resource_metrics(instance_id: Annotated[str, typer.Option(help="RDS database instance id")]):
+    logger.info(f"Hello {instance_id}")
+
+
+@app.command()
+def goodbye(name: str, formal: bool = False):
+    if formal:
+        print(f"Goodbye Ms. {name}. Have a good day.")
+    else:
+        print(f"Bye {name}!")
 
 
 if __name__ == "__main__":
-    typer.run(main)
+    app()
